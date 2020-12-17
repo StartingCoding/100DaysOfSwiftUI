04324bc58b72346fb449bb6172d8cc8e817ea9ab
StartingCoding <StartingCodingPartner@gmail.com>
2020-12-17 11:26:23 +0100

    Day 1

---
+++ Day 1/Day1.playground/Contents.swift
@@ -0,0 +1,20 @@
+import Foundation
+
+let constant = "This is a constant"
+var variable = "This holds a value that can change"
+
+let thisIsaString: String = "1"
+let multiLineString = """
+This string
+can goes
+on more lines
+"""
+
+let thisIsaWholeNumber: Int = 1
+let thisIsaFractionalNumber: Double = 1.0
+
+let booleansCouldBeTrue = true
+let booleansCouldBeFalse = false
+
+let dynamicValue = "different values"
+let stringCanHaveInterpolation = "This string can have a \(dynamicValue)"
